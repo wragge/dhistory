d69567e3255dd748015b3a3a8cd6ab3a157647cc
Tim Sherratt <tim@discontents.com.au>
2013-11-10 00:30:28 +1100

    Switch caching back on

--- dhistory/collectionviews/views.py
+++ dhistory/collectionviews/views.py
@@ -49,7 +49,7 @@ JSON_SCHEMA = {
 START_YEAR = 1900
 END_YEAR = 2013
 
-#@cache_page(60 * 60 * 6)
+@cache_page(60 * 60 * 6)
 def create_collectionview(request):
     preset = "false"
     selected_nucs = request.GET.getlist('nuc')
