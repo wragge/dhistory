acd02237fd03af8f7ef94d53f31a2df1a050da71
Tim Sherratt <tim@discontents.com.au>
2020-06-26 18:26:43 +1000

    Update side links as well

--- dhistory/querypic/static/js/qp-show-script.js
+++ dhistory/querypic/static/js/qp-show-script.js
@@ -233,11 +233,11 @@ $(function(){
     }
     function rewrite_trove_query(query) {
         // https://trove.nla.gov.au/newspaper/result?requestHandler=%2FtextSearch&dateFrom=1860-01-01&dateTo=1879-12-31&q=%22victorian+rules%22+%22rules+football%22%7E5&fromyyyy=1866&toyyyy=1866
-        query = query.replace(/newspaper\/result?/, 'search/category/newspapers?');
-        query = query.replace(/requestHandler=%2FtextSearch/, '');
-        query = query.replace(/&dateFrom/, '&date.from');
-        query = query.replace(/&dateTo/, '&date.to');
-        query = query.replace(/&q=/, 'keyword=');
+        query = query.replace('newspaper/result?', 'search/category/newspapers?');
+        query = query.replace('requestHandler=%2FtextSearch', '');
+        query = query.replace('&dateFrom', '&date.from');
+        query = query.replace('&dateTo', '&date.to');
+        query = query.replace('&q=', '&keyword=');
         return query
     }
     function show_trove_articles(results, query_date, series) {
